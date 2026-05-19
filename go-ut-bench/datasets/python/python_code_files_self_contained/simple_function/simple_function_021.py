6e1c90a6bfe30854906b42f9620a3b06dc1ee562
rojim666 <771473941@qq.com>
2026-05-19 15:52:01 +0800

    delete dataset

--- go-ut-bench/datasets/python/python_code_files_self_contained/simple_function/simple_function_021.py
+++
@@ -1,9 +0,0 @@
-import re
-import string
-from random import choice
-
-def task_func(n, pattern):
-    while True:
-        s = ''.join((choice(string.ascii_letters) for _ in range(n)))
-        if re.match(pattern, s):
-            return s
--- go-ut-bench/datasets/python/python_code_files_self_contained/simple_function/simple_function_040.py
+++
@@ -1,9 +0,0 @@
-from functools import reduce
-import operator
-import string
-
-def task_func(letters):
-    letter_to_number = {letter: i + 1 for i, letter in enumerate(string.ascii_uppercase)}
-    numbers = [letter_to_number[letter] for letter in letters]
-    product = reduce(operator.mul, numbers, 1)
-    return product
